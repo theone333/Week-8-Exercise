90d30b6e868095226fc3e12b2ab59cddb8175408
theone333 <32265996+theone333@users.noreply.github.com>
2017-12-04 17:45:41 +0000

    Create Week8 - Exercise8.py

---
+++ Week8 - Exercise8.py
@@ -0,0 +1,14 @@
+def main():
+
+    f = 2
+
+def f(x) :
+    return g(x) + math.sqrt(h(x))
+def g(x) :
+    return 4 * h(x)
+def h(x) :
+    return x * x + k(x) - 1
+def k(x) :
+    return 2 * (x + 1)
+
+main()
